60a51238ca229ab3342c5b9637fe3c3e20ece834
isternberg <i.sternberg@posteo.de>
2016-07-24 17:32:23 +0200

    refactored validator and added tests

---
+++ src/test/java/com/goeuro/validation/InputValidatorTest.java
@@ -0,0 +1,13 @@
+package com.goeuro.validation;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class InputValidatorTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
